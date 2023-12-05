9d14f841261aab284e234fab972d114508ffd2a7
VictorRait <victorlemuelrait@gmail.com>
2023-12-05 18:34:41 +0800

    add socialapp and servicepage

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -4,6 +4,24 @@ import ProjectListItem from "./ProjectListItem";
 
 const works = [
   {
+    name: "Social App",
+    tag: ["all", "html", "react", "react-router", "redux", "formik", "MUI"],
+    description:
+      "A Social App, a simple social media application in which you can post about things that you like add friends in your friend list. --Features to add next: video and audio postings, link to other platforms,  notifications and messaging.  ",
+    image: "../assets/projects_thumbnails/sociopedia.png",
+    codeLink: "https://github.com/VictorRait/SocialApp",
+    demoLink: "https://socialapp-i72u.onrender.com/",
+  },
+  {
+    name: "Service page for real estate",
+    tag: ["all", "html", "css"],
+    description:
+      "I was given a task to revamp a service page, and I tried to preserve the aesthetics of the whole site to make not it out of place while still making not as boring as possible.  (as of Dec.4,23) before:https://hansenpartners.net/services ",
+    image: "../assets/projects_thumbnails/service_page.png",
+    codeLink: "https://github.com/VictorRait/real-estate-service-page",
+    demoLink: "https://mellow-swan-db29b7.netlify.app/",
+  },
+  {
     name: "Weather App",
     tag: ["react", "all", "expo", "react-native"],
     description:
